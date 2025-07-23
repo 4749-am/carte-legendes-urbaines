12853ee82ad7386d5d56a558cd3997f065ed057e
git config --global user.name Anne-Marie <amm47home@gmail.com>
2025-07-23 19:18:26 +0200

    mapview component created

---
+++ frontend/src/components/MapView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from 'vue';
+import L from "leaflet";
+import 'leaflet/dist/leaflet.css';
+
+    onMounted(() => {
+        const map = L.map('map').setView([48.866669, 2.33333], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([48.866669, 2.33333]).addTo(map)
+        .bindPopup('Une jolie popup.<br> Facilement customisable.')
+        .openPopup();
+    });  
+</script>
+
+<template>
+    <div id="map"></div>
+</template>
+
+<style scoped>
+    #map {
+        height: 50vh;
+        width: 100%;
+        border-radius: 15px;
+    }
+</style>
